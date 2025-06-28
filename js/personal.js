d9a1a73ee077f2c897eab0ad79618d59387b53fa
ema-lei <ema.lei.168@gmail.com>
2025-06-28 00:00:25 -0700

    Animated topic switch in personal and added js needed to switch articles when button clicked

---
+++ js/personal.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll('.topic');
+const articles = document.querySelectorAll('.topic-article');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        // deactivate all tabs + hide all articles
+        tabs.forEach(t => t.classList.remove('active'));
+        articles.forEach(a => a.classList.add('hidden'));
+
+        // activate the clicked tab + show its article
+        tab.classList.add('active');
+        document.getElementById(tab.dataset.name).classList.remove('hidden');
+    });
+});
